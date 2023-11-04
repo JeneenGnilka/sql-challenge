6e2163893bb13928121bac24254e0f21a4fe5a3c
JeneenGnilka <142128170+JeneenGnilka@users.noreply.github.com>
2023-11-04 16:21:06 -0400

    updated sql challenge homework

---
+++ table_schemata.sql
@@ -0,0 +1,61 @@
+
+-- create table for departments.csv
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR NOT NULL,
+    "dept_name" VARCHAR NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY ("dept_no")
+);
+
+-- create table for dept_emp.csv
+CREATE TABLE "dept_emp" (
+    "emp_no" INTEGER NOT NULL,
+    "dept_no" VARCHAR NOT NULL
+);
+
+-- create table for dept_manager.csv
+CREATE TABLE "dept_manager" (
+    "dept_no" VARCHAR NOT NULL,
+    "emp_no" INTEGER NOT NULL
+);
+
+-- create table for employees.csv
+CREATE TABLE "employees" (
+    "emp_no" INTEGER NOT NULL,
+    "emp_title_id" VARCHAR NOT NULL,
+    "birth_date" VARCHAR NOT NULL, 
+    "first_name" VARCHAR NOT NULL,
+    "last_name" VARCHAR NOT NULL,
+    "sex" VARCHAR NOT NULL,
+    "hire_date" VARCHAR NOT NULL
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" INTEGER NOT NULL,
+    "salary" INTEGER NOT NULL,  
+    CONSTRAINT "pk_salaries" PRIMARY KEY ("emp_no")
+);
+
+-- create table for titles.csv
+CREATE TABLE "titles" (
+    "title_id" VARCHAR NOT NULL,
+    "title" VARCHAR NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY ("title_id")
+);
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY ("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY ("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY ("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY ("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY ("emp_title_id")
+REFERENCES "titles" ("title_id");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY ("emp_no")
+REFERENCES "employees" ("emp_no");
